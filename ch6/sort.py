29ccbb65ac14af7a03afa156818a77f9744f317c
tomagent <tommymora2003@gmail.com>
2021-05-09 21:15:48 -0500

    Document algorithm

--- ch6/sort.py
+++ ch6/sort.py
@@ -1,11 +1,11 @@
-# Define function to make a bubble sort algorithm
+# Define function to make a bubble sort algorithm with two parameters (scores and the indexes)
 def bubble_sort(scores, numbers):
     swapped = True # Set swapped variable to True to begin the loop
 
     while swapped: 
         swapped = False 
         for i in range(0, len(scores)-1): # Loop from 0 to the length of scores minus one  
-            if scores[i] < scores[i+1]: # We compare if the score at given index is less than the next score
+            if scores[i] < scores[i+1]: # We compare if the score at given index is less than the next score, then modify the lists
                 temp = scores[i]
                 scores[i] = scores[i+1]
                 scores[i+1] = temp
@@ -16,11 +16,11 @@ def bubble_sort(scores, numbers):
 
 scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44]
 
-number_of_scores = len(scores)
-solution_numbers = list(range(number_of_scores))
-bubble_sort(scores, solution_numbers)
+number_of_scores = len(scores) # Get length of scores
+solution_numbers = list(range(number_of_scores)) # Make a list from 0 to 35
+bubble_sort(scores, solution_numbers) # Pass the scores and the solution list to the function
 
 print("Top Bubble Solutions")
-for i in range(5):
+for i in range(5): # Loop 5 times to get the top 5 scores with its index
     print(str(i+1) + ")",
           "Bubble solution #" + str(solution_numbers[i]), "score:", scores[i])
