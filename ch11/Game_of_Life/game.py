ced0b6c875a1092d7a08e30b1847e9d3a8677498
tomagent <tommymora2003@gmail.com>
2021-06-01 20:45:38 -0500

    Add controller and comments

--- ch11/Game_of_Life/game.py
+++ ch11/Game_of_Life/game.py
@@ -1,8 +1,10 @@
 from random import randint
 
+# Dimensions of the grid
 height = 100
 width = 100
 
+# Add random cells (live (1) or dead (0))
 def randomize(grid, width, height):
     for i in range(0, height):
         for j in range(0, width):
@@ -21,24 +23,32 @@ randomize(grid_model, width, height)
 def next_gen():
     global grid_model, next_grid_model
 
+    # Count the neighbors in each cell
     for i in range(0, height):
         for j in range(0, width):
             cell = 0
             count = count_neighbors(grid_model, i, j)
-
+            # If it's dead
             if grid_model[i][j] == 0:
+                # And the count is 3
                 if count == 3:
+                    # Revive it 
                     cell = 1
+            # If it's alive
             elif grid_model[i][j] == 1:
+                # If it either has 2 or 3 neighbors
                 if count == 2 or count == 3:
+                    # Let it live
                     cell = 1
+            # Set that cell in the next gen as the previous
             next_grid_model[i][j] = cell
             
-
+    # Set the present grid to the next one
     temp = grid_model
     grid_model = next_grid_model
     next_grid_model = temp
 
+# Count adjacent neighbors
 def count_neighbors(grid, row, col):
     count = 0
     if row-1 >= 0:
--- ch11/Game_of_Life/view.py
+++ ch11/Game_of_Life/view.py
@@ -6,24 +6,59 @@ cell_size = 5
 def setup():
     global root, grid_view, cell_size, start_button, clear_button, choice
     
+    # Create GUI
     root = Tk()
     root.title("The Game of Life")
 
+    # Set size
     grid_view = Canvas(root, width=game.width*cell_size, height=game.height*cell_size, borderwidth=0, highlightthickness=0, bg="white")
 
+    # Add start and clear button
     start_button = Button(root, text="Start", width=12)
     clear_button = Button(root, text="Clear", width=12)
 
+    # Option menu
     choice = StringVar(root)
     choice.set("Choose a Pattern")
     option = OptionMenu(root, choice, "Choose a Pattern", "glider", "glider gun", "random")
     option.config(width=20)
 
+    # Layout
     grid_view.grid(row=0, columnspan=3, padx=20, pady=20)
     start_button.grid(row=1, column=0, sticky=W, padx=20, pady=20)
     option.grid(row=1, column=1, padx=20)
     clear_button.grid(row=1, column=2, sticky=E, padx=20, pady=20)
-   
+
+def update():
+    global grid_view
+
+    # Delete anything drawn on the canvas
+    grid_view.delete(ALL)
+    # Compute the next gen
+    game.next_gen()
+    # Iterate through every cell
+    for i in range(0, game.height):
+        for j in range(0, game.width):
+            if game.grid_model[i][j] == 1: # If a cell at i, j is live, a black rectangle is drawn
+                draw_cell(i, j, "black")
+
+
+def draw_cell(row, col, color):
+    global grid_view, cell_size
+
+    # Aesthetic details
+    if color == "black":
+        outline = "grey"
+    else:
+        outline = "white"
+    
+    # Draw the rectangle
+    grid_view.create_rectangle(row*cell_size, col*cell_size, row*cell_size+cell_size, col*cell_size+cell_size, fill=color, outline=outline)
+
+    
+
+
 if __name__ == "__main__":
     setup()
+    update()
     mainloop()
