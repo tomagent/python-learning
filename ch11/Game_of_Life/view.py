be9283d2b5ae5c3cb002e0930ae201eefa7a908c
tomagent <tommymora2003@gmail.com>
2021-06-01 22:35:11 -0500

    add bind and after events

--- ch11/Game_of_Life/view.py
+++ ch11/Game_of_Life/view.py
@@ -2,6 +2,7 @@ from tkinter import *
 import game
 
 cell_size = 5
+is_running = False
 
 def setup():
     global root, grid_view, cell_size, start_button, clear_button, choice
@@ -15,6 +16,9 @@ def setup():
 
     # Add start and clear button
     start_button = Button(root, text="Start", width=12)
+    # Call start_handler when clicked
+    start_button.bind("<Button-1>", start_handler)
+
     clear_button = Button(root, text="Clear", width=12)
 
     # Option menu
@@ -29,8 +33,20 @@ def setup():
     option.grid(row=1, column=1, padx=20)
     clear_button.grid(row=1, column=2, sticky=E, padx=20, pady=20)
 
+def start_handler(event):
+    global is_running, start_button
+
+    if is_running:
+        is_running = False
+        start_button.configure(text="Start")
+    else:
+        is_running = True
+        start_button.configure(text="Pause")
+        update()
+    
+
 def update():
-    global grid_view
+    global grid_view, root, is_running
 
     # Delete anything drawn on the canvas
     grid_view.delete(ALL)
@@ -41,6 +57,9 @@ def update():
         for j in range(0, game.width):
             if game.grid_model[i][j] == 1: # If a cell at i, j is live, a black rectangle is drawn
                 draw_cell(i, j, "black")
+    # Call update in 100 miliseconds
+    if (is_running):
+        root.after(100, update)
 
 
 def draw_cell(row, col, color):
