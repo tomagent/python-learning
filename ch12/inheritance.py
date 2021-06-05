85828126766a833d2f8402839fd4c7fe60564688
tomagent <tommymora2003@gmail.com>
2021-06-05 16:52:48 -0500

    inheritance

--- ch12/inheritance.py
+++ ch12/inheritance.py
@@ -19,8 +19,18 @@ class Dog:
 def print_dog(dog):
     print(dog.name + "'s", "age is", dog.age, "and weight is", dog.weight)
 
+class ServiceDog(Dog):
+    def __init__(self, name, age, weight, handler):
+        Dog.__init__(self, name, age, weight)
+        self.handler = handler
+
+    def walk(self):
+        print(f"{self.name} is helping its handler {self.handler} walk")
+
 if __name__ == "__main__":
-    codie = Dog("Codie", 12, 38)
-    jackson = Dog("Jackson", 9, 12)
-    print(f"{jackson.name}'s age in human years is {jackson.human_years()}") # String interpolation
-    print(f"{codie.name}'s age in human years is {codie.human_years()}")
+    rody = ServiceDog("Rody", 8, 38, "Joseph")
+    print(f"This dog's name is {rody.name}")
+    print(f"This dog's handler is {rody.handler}")
+    print_dog(rody)
+    rody.bark()
+    rody.walk()
