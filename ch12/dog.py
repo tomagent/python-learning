e85e53eb585ba32d1c6c306c47592a515c4d5640
tomagent <tommymora2003@gmail.com>
2021-06-07 12:47:20 -0500

    dog method

--- ch12/dog.py
+++ ch12/dog.py
@@ -1,19 +1,28 @@
+# Create new class named Dog
 class Dog:
+    # Dog constructor with name, age and weight as parameters
     def __init__(self, name, age, weight):
         self.name = name
         self.age = age
         self.weight = weight
 
+    # Bark method
     def bark(self):
         if self.weight > 29:
             print(self.name, 'says "WOOF WOOF"')
         else:
             print(self.name, 'says "woof woof"')
 
+    # Get human years
     def human_years(self):
         human_age = self.age * 7
         return human_age
 
+    # Walk 
+    def walk(self):
+        print(f"{self.name} is walking")
+
+    # Object itself
     def __str__(self):
         return f"I'm a dog name {self.name}"
 
