bb04529054287d82539f3e8fb70f8be8d5ad91a9
tomagent <tommymora2003@gmail.com>
2021-06-07 11:51:30 -0500

    document code

--- ch12/inheritance.py
+++ ch12/inheritance.py
@@ -1,63 +1,91 @@
+# Import dog class
 from dog import Dog
 
+# Create a servicedog class inherited from "Dog"
 class ServiceDog(Dog):
+    # ServiceDog constructor
     def __init__(self, name, age, weight, handler):
+        # Dog constructor
         Dog.__init__(self, name, age, weight)
+        # Define servicedog attributes
         self.handler = handler
         self.is_working = False # Define attribute (not necessary it needs to mirror the parameter)
 
+    # Walk
     def walk(self):
         print(f"{self.name} is helping its handler {self.handler} walk")
 
+    # Bark
     def bark(self):
+        # Override bark method
         if self.is_working:
             print(f"{self.name} says I can't bark I'm working")
         else: 
+            # Call dog bark method
             Dog.bark(self)
 
+# Create frisbee class
 class Frisbee:
+    # Constructor and define attributes
     def __init__(self, color):
         self.color = color
 
+    # Object itself as string
     def __str__(self):
         return f"I'm a {self.color} frisbee"
 
+# Create frisbeedog inherited with dog
 class FrisbeeDog(Dog):
+    # FrisbeeDog constructor
     def __init__(self, name, age, weight):
+        # Dog constructor
         Dog.__init__(self, name, age, weight)
+        # Define other attributes
         self.frisbee = None
 
+    # Bark
     def bark(self):
+        # If the dog has a frisbee
         if self.frisbee != None:
             print(f"{self.name} says I can't bark, I have a frisbee in my mouth")
         else:
             Dog.bark(self)
 
+    # Catch
     def catch(self, frisbee):
         self.frisbee = frisbee
         print(f"{self.name} caught a {frisbee.color} frisbee")
 
     def give(self):
+        # If the dog has a frisbee
         if self.frisbee != None:
+            # Temp frisbee
             frisbee = self.frisbee
+            # Set frisbee to none
             self.frisbee = None
+            # Gibve back
             print(f"{self.name} gives back {frisbee.color} frisbee")
             return frisbee
         else:
             print(f"{self.name} doesn't have a frisbee")
             return None
 
+    # Object as string
     def __str__(self):
         str = f"I'm a dog named {self.name}"
+        # If it has a frisbee
         if self.frisbee != None:
             str = f"{str} and I have a frisbee"
         return str
 
+# Hotel object
 class Hotel:
     def __init__(self, name):
         self.name = name
+        # Dictionary
         self.kennel = {}
 
+    # Check in only dogs
     def check_in(self, dog):
         if isinstance(dog, Dog):
             self.kennel[dog.name] = dog
@@ -65,6 +93,7 @@ class Hotel:
         else:
             print(f"Sorry only Dogs are allowed in {self.name}")
 
+    # Check out dogs already checked in
     def check_out(self, name):
         if name in self.kennel:
             dog = self.kennel[name]
@@ -73,8 +102,9 @@ class Hotel:
             return dog
         else:
             print(f"Sorry {name} is not boarding at {self.name}")
-            return None
-
+            return 
+            
+# Cat object
 class Cat():
     def __init__(self, name):
         self.name = name
@@ -82,6 +112,7 @@ class Cat():
     def meow(self):
         print(f"{self.name} says Meow")
 
+# Testing :)
 def test_code():
     codie = Dog("Codie", 12, 38)
     jackson = Dog("Jackson", 9, 12)
@@ -109,11 +140,3 @@ def test_code():
 
 if __name__ == "__main__":
     test_code()
-    # rody = ServiceDog("Rody", 8, 38, "Joseph")
-    # print(f"This dog's named is {rody.name}")
-    # print(f"This dog's handler is {rody.handler}")
-    # print(rody)
-    # rody.bark()
-    # rody.is_working = True
-    # rody.bark()
-    # rody.walk()
