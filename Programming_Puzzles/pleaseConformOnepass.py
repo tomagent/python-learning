4673fac8ee57c1cc43432e45e19662a9c09e450d
tomagent <tommymora2003@gmail.com>
2021-06-13 18:07:41 -0500

    natural commands

--- Programming_Puzzles/pleaseConformOnepass.py
+++ Programming_Puzzles/pleaseConformOnepass.py
@@ -2,9 +2,16 @@ def pleaseConformOnepass(caps):
     # Ensure last interval is printed in case last element is different than the first
     caps = caps + [caps[0]]
     for i in range(1, len(caps)):
-        # New interval
         if caps[i] != caps[i-1]:
             if caps[i] != caps[0]:
-                print('People in positions', i, end='')
+                start = i
             else:
-                print(' through', i-1, "flip your caps!")
+                if start == i-1:
+                    print(f"Person at position {start} please flip your cap!")
+                else:
+                    print(f"People in positions {start} through {i-1} flip your cap! ")
+
+if __name__ == "__main__":
+    cap1 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "B", "F"]
+    cap2 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "F", "F"]
+    pleaseConformOnepass(cap1)
