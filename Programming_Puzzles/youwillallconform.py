89707aabf1d50a4120b398b2da1f071bec9683a5
tomagent <tommymora2003@gmail.com>
2021-06-13 18:39:24 -0500

    skip bareheaded

--- Programming_Puzzles/youwillallconform.py
+++ Programming_Puzzles/youwillallconform.py
@@ -2,9 +2,13 @@ def pleaseConform(caps):
     # Initialize
     start = forward = backward = 0
     intervals = []
+    caps = caps + ["END"]
 
     #Determine intervals where caps are on in the same direction
     for i in range(1, len(caps)):
+        if caps[i-1] == "H":
+            start = i
+
         if caps[start] != caps[i]:
             # each interval is a tuple with 3 elements (start, end, type)
             intervals.append((start, i - 1, caps[start]))
@@ -14,13 +18,6 @@ def pleaseConform(caps):
             else:
                 backward += 1
             start = i
-
-    #Need to add the last interval after for loop completes execution
-    intervals.append((start, len(caps) - 1, caps[start]))
-    if caps[start] == 'F':
-        forward += 1
-    else:
-        backward += 1
 
     # The minority need to flip their cap
     if forward < backward:
@@ -39,5 +36,6 @@ def pleaseConform(caps):
 
 if __name__ == "__main__":
     cap1 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "B", "F"]
-    pleaseConform(cap1)
+    cap3 = ["F", "F", "B", "H", "B", "F", "B", "B", "B", "F", "H", "F", "F"]
+    pleaseConform(cap3)
 
